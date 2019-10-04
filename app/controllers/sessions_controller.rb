bc8ab59d827d8b61f034a894e16b23d38f7fd634
Bonny Mogollon <bonny.mogollon11@gmail.com>
2019-10-03 20:23:03 -0400

    returning to styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,28 @@
 class SessionsController < ApplicationController
   def new
 
-    render :layout => false
+      render :layout => false
 
-  end
-
-  def create
-    user = Admin.find_by(email: params[:session][:email].downcase)
-      if user && user.authenticate(params[:session][:password])
-
-        log_in user
-        redirect_to '/admins'
-      else
-        flash.now[:danger] = 'Invalid email/password combination'
-      end
     end
 
-def destroy
-user = current_user
+    def create
+      user = Admin.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
 
-   log_out user
-   redirect_to root_path
- end
+          log_in user
+          redirect_to '/admins'
+        else
+          flash.now[:danger] = 'Invalid email/password combination'
+        end
+      end
+
+  def destroy
+  user = current_user
+
+     log_out user
+     redirect_to root_path
+   end
+
 
 
 end
